80252a48c29d7e1c8b7cd2d301845d0d4b88ed40
Anthony Nnanna <anthonynnanna004@gmail.com>
2025-07-10 14:23:40 +0100

    overview

--- resources/views/trader/overview.blade.php
+++ resources/views/trader/overview.blade.php
@@ -1,6 +1,6 @@
 <x-trader-layout>
     <div>
-        <!-- Live Crypto Ticker Tape Marquee -->
+        {{-- Live Crypto Ticker Tape Marquee --}}
         <div class="w-full bg-white text-black overflow-hidden shadow-lg my-10">
             <div class="tradingview-widget-container">
                 <script type="text/javascript" src="https://s3.tradingview.com/external-embedding/embed-widget-ticker-tape.js" async>
@@ -35,6 +35,62 @@
                 </script>
             </div>
         </div>
+
+        {{-- Second Div --}}
+        <div class="overview-second-div-con">
+            <div>
+                <div class="overview-second-div-user-con">
+                    <div>
+                        <img src="{{asset('img/user-2.png')}}" alt="user" class="overview-second-div-user-img">
+                    </div>
+                    <div>
+                        <div class="overview-second-div-salute">Hi, Conor</div>
+                        <div class="overview-second-div-salute-text">Here's a summary of your account. Have fun!</div>
+                    </div>
+                </div>
+                <div class="overview-second-div-btn-con">
+                    <a href="{{ route('trader.deposits') }}">
+                        <div class="overview-second-div-deposit-btn">
+                            <span class="icon-[tabler--home-dollar] text-xl"></span>
+                            <span>Deposit</span>
+                        </div>
+                    </a>
+                    <a href="{{ route('trader.withdrawals') }}">
+                        <div class="overview-second-div-withdraw-btn">
+                            <span class="icon-[tabler--credit-card] text-xl"></span>
+                            <span>Withdraw</span>
+                        </div>
+                    </a>
+                </div>
+            </div>
+            <div>
+                <img src="{{asset('img/overview-img1.svg')}}" alt="">
+            </div>
+        </div>
+
+        {{-- Third div --}}
+        <div class="overview-third-div-first">
+            <div class="overview-third-div-second">
+                <div>
+                    <span class="icon-[tabler--info-circle]"></span>
+                    <span>Verify KYC Information to activate withdrawals</span>
+                </div>
+                <a href="">
+                    <div class="overview-third-div-third">Verify KYC</div>
+                </a>
+            </div>
+            <div class="overview-third-div-fourth">
+                <div>
+                    <span class="icon-[tabler--info-circle]"></span>
+                    <span>Update your account information from your profile to make withdrawals.</span>
+                </div>
+                <a href="">
+                    <div class="overview-third-div-fifth">Update Profile</div>
+                </a>
+            </div>
+        </div>
+
+        {{-- overview fourth div --}}
         <div class="grid grid-cols-1 md:grid-cols-3 gap-10">
             <!-- Bitcoin Live Price Card -->
             <div class="overview-chart-card-con">
@@ -49,7 +105,7 @@
                             "height": "100",
                             "locale": "en",
                             "dateRange": "1D",
-                            "colorTheme": "light",
+                            "colorTheme": "dark",
                             "trendLineColor": "#2196F3",
                             "underLineColor": "rgba(33, 150, 243, 0.15)",
                             "isTransparent": false,
@@ -61,7 +117,7 @@
                 </div>
             </div>
 
-            <!-- Bitcoin Live Price Card -->
+            <!-- Solana Live Price Card -->
             <div class="overview-chart-card-con">
                 <div class="text-center font-semibold mb-2">Live SOL/USDT Price</div>
                 <div class="flex justify-center">
@@ -74,7 +130,7 @@
                             "height": "100",
                             "locale": "en",
                             "dateRange": "1D",
-                            "colorTheme": "light",
+                            "colorTheme": "dark",
                             "trendLineColor": "#2196F3",
                             "underLineColor": "rgba(33, 150, 243, 0.15)",
                             "isTransparent": false,
@@ -86,7 +142,7 @@
                 </div>
             </div>
 
-            <!-- Bitcoin Live Price Card -->
+            <!-- Ethereum Live Price Card -->
             <div class="overview-chart-card-con">
                 <div class="text-center font-semibold mb-2">Live ETH/USDT Price</div>
                 <div class="flex justify-center">
@@ -99,7 +155,7 @@
                             "height": "100",
                             "locale": "en",
                             "dateRange": "1D",
-                            "colorTheme": "light",
+                            "colorTheme": "dark",
                             "trendLineColor": "#2196F3",
                             "underLineColor": "rgba(33, 150, 243, 0.15)",
                             "isTransparent": false,
@@ -110,6 +166,170 @@
                     </div>
                 </div>
             </div>
+        </div>
+
+        {{-- fifth div --}}
+        <div class="overview-fifth-div-con">
+            <div class="overview-fifth-div-card-con">
+                <div>
+                    <div class="overview-fifth-div-card-header">Available Balance</div>
+                    <div class="overview-fifth-div-card-left">
+                        <span class="overview-fifth-div-card-balance">$0.00</span>
+                        <span class="overview-fifth-div-card-arrow">
+                            <span class="icon-[tabler--arrow-up-left] text-green-400"></span>
+                        </span>
+                        <span>+ 9%</span>
+                    </div>
+                </div>
+                <div>
+                    <img src="{{ asset('img/overview-img2.svg') }}" alt="" class="overview-fifth-div-card-img">
+                </div>
+            </div>
+            <div class="overview-fifth-div-card-con">
+                <div>
+                    <div class="overview-fifth-div-card-header">Total Deposit</div>
+                    <div class="overview-fifth-div-card-left">
+                        <span class="overview-fifth-div-card-balance">$0.00</span>
+                        <span class="overview-fifth-div-card-arrow">
+                            <span class="icon-[tabler--arrow-up-left] text-green-400"></span>
+                        </span>
+                        <span>+ 9%</span>
+                    </div>
+                </div>
+                <div>
+                    <img src="{{ asset('img/overview-img2.svg') }}" alt="" class="overview-fifth-div-card-img">
+                </div>
+            </div>
+            <div class="overview-fifth-div-card-con">
+                <div>
+                    <div class="overview-fifth-div-card-header">Total Withdrawal</div>
+                    <div class="overview-fifth-div-card-left">
+                        <span class="overview-fifth-div-card-balance">$0.00</span>
+                        <span class="overview-fifth-div-card-arrow">
+                            <span class="icon-[tabler--arrow-up-left] text-green-400"></span>
+                        </span>
+                        <span>+ 9%</span>
+                    </div>
+                </div>
+                <div>
+                    <img src="{{ asset('img/overview-img2.svg') }}" alt="" class="overview-fifth-div-card-img">
+                </div>
+            </div>
+        </div>
+
+        {{-- sixth div --}}
+        <div class="overview-sixth-div-con">
+            <div class="overview-sixth-div-left-con">
+                <div>
+                    <div class="overview-sixth-div-wrap-one">
+                        <span class="overview-sixth-div-wrap-two">
+                            <span class="overview-sixth-div-wrap-three">
+                                <span class="icon-[token--eth] overview-sixth-div-wrap-four"></span>
+                            </span>
+                        </span>
+                        <span class="font-bold">TOKEN BALANCE</span>
+                    </div>
+                    <div class="overview-sixth-div-wrap-five">0 ELT</div>
+                </div>
+                <div>
+                    <div class="overview-sixth-div-wrap-six button">
+                        <span class="icon-[tabler--ticket]"></span>
+                        <span>Load Tokens</span>
+                    </div>
+                    <div class="overview-sixth-div-wrap-seven button">Purchase Tokens</div>
+                </div>
+            </div>
+
+            <div class="overview-sixth-div-right-con">
+                <div>
+                    <div class="overview-sixth-div-wrap-eight">
+                        <img src="{{ asset('img/overview-img2.svg') }}" alt="" class="overview-sixth-div-wrap-nine">
+                        <span class="font-bold">TOKEN BALANCE</span>
+                    </div>
+                    <div class="overview-sixth-div-wrap-ten">$0</div>
+                </div>
+                <div class="overview-sixth-div-wrap-eleven">
+                    <span class="icon-[tabler--trending-up]"></span>
+                    <span>+3.20%</span>
+                </div>
+            </div>
+        </div>
+
+        {{-- seventh div --}}
+        <div class="overview-seventh-div-con">
+            <div class="overview-seventh-div-wrap-one">
+                <div class=" ">
+                    <img src="{{asset('img/overview-img4.png')}}" alt="bot" class="overview-seventh-div-img">
+                </div>
+                <div class="overview-seventh-div-wrap-two">Bot is not active</div>
+                <div>Please activate the bot to start trading</div>
+            </div>
+        </div>
+
+        {{--eight div --}}
+        <div>
+            <!-- TradingView Top Stories / News Widget -->
+            <div class="overview-chart-card-con">
+                <div class="text-center mb-2 font-extrabold text-2xl">#Market Top Stories</div>
+                <div class="flex justify-center">
+                    <div class="tradingview-widget-container">
+                        <div class="tradingview-widget-container__widget"></div>
+                        <script type="text/javascript" src="https://s3.tradingview.com/external-embedding/embed-widget-timeline.js" async>
+                        {
+                            "feedMode": "market",
+                            "colorTheme": "light",
+                            "isTransparent": false,
+                            "displayMode": "regular",
+                            "width": "100%",
+                            "height": 400,
+                            "locale": "en"
+                        }
+                        </script>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        {{-- ninth div --}}
+        <div class="md:w-5/6 mx-auto my-8">
+            <div class="flex justify-between items-center my-5">
+                <div class="font-bold md:text-xl">Recent Transactions</div>
+                <a href="{{route('trader.transactions')}}">
+                    <div class="border border-blue-400 text-blue-400 inline-flex items-center gap-1 p-2 rounded-lg hover:text-white hover:bg-blue-400 font-bold button">
+                        <span>View all</span>
+                        <span class="icon-[tabler--arrow-big-right-lines-filled] md:text-xl"></span>
+                    </div>
+                </a>
+            </div>
+            <div class="h-30 overflow-auto">
+                <table class="mx-auto table-auto w-full border border-gray-200 text-gray-500">
+                    <thead>
+                        <tr class="border-b border-gray-300">
+                            <th class="border-none px-4 py-2 text-center">Transaction</th>
+                            <th class="border-none px-4 py-2 text-center">Status</th>
+                            <th class="border-none px-4 py-2 text-center">Date</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td class="border-none px-4 py-2 text-center">Deposit</td>
+                            <td class="border-none px-4 py-2 text-center">Success</td>
+                            <td class="border-none px-4 py-2 text-center">Monday</td>
+                        </tr>
+                        <tr>
+                            <td class="border-none px-4 py-2 text-center">Deposit</td>
+                            <td class="border-none px-4 py-2 text-center">Success</td>
+                            <td class="border-none px-4 py-2 text-center">Monday</td>
+                        </tr>
+                        <tr>
+                            <td class="border-none px-4 py-2 text-center">Deposit</td>
+                            <td class="border-none px-4 py-2 text-center">Success</td>
+                            <td class="border-none px-4 py-2 text-center">Monday</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            
         </div>
     </div>
 </x-trader-layout>

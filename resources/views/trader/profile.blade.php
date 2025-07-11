daa6fafe48d84072229f8876c8dd670bb13e36ab
Anthony Nnanna <anthonynnanna004@gmail.com>
2025-07-11 18:48:37 +0100

    profile start

--- resources/views/trader/profile.blade.php
+++ resources/views/trader/profile.blade.php
@@ -1,3 +1,18 @@
 <x-trader-layout>
-    <div></div>
+    <div>
+        {{-- first div --}}
+        <div class="flex justify-between items-center bg-purple-50 px-7 py-5 rounded-xl">
+            <div class="basis-2/3">
+                <div class="text-2xl font-bold">Profile</div>
+                <div class="text-xs md:text-base inline-flex items-center">
+                    <span>Dashboard</span>
+                    <span class="icon-[ph--dot] text-3xl font-bold"></span>
+                    <span>Profile</span>
+                </div>
+            </div>
+            <div class="basis-1/3 flex justify-end">
+                <img src="{{asset('img/profile-img-one.png')}}" alt="profile-img-1">
+            </div>
+        </div>
+    </div>
 </x-trader-layout>

be42c299624c6bb8a1238c411655cff390fea5da
Anthony Nnanna <anthonynnanna004@gmail.com>
2025-07-13 21:13:33 +0100

    initial second div

--- resources/views/trader/profile.blade.php
+++ resources/views/trader/profile.blade.php
@@ -14,5 +14,29 @@
                 <img src="{{asset('img/profile-img-one.png')}}" alt="profile-img-1">
             </div>
         </div>
+
+        {{-- second div --}}
+        <div class="profile-second-div-con">
+            <a href="{{route('trader.profile')}}" class="profile-second-div-wrap-one {{ request()->routeIs('trader.profile') ? 'border-blue-600 text-blue-600' : 'text-gray-600 border-white' }}">
+                <div>
+                    <span class="icon-[tabler--user-circle] profile-second-div-wrap-two"></span>
+                </div>
+                <div>Profile</div>
+            </a>
+            
+            <a href="" class="profile-second-div-wrap-one {{ request()->routeIs('trader.profile.accounts') ? 'border-blue-600 text-blue-600' : 'text-gray-600 border-white' }}">
+                <div>
+                    <span class="icon-[tabler--building-bank] profile-second-div-wrap-two"></span>
+                </div>
+                <div>Accounts</div>
+            </a>
+
+            <a href="" class="profile-second-div-wrap-one {{ request()->routeIs('trader.profile.security') ? 'border-blue-600 text-blue-600' : 'text-gray-600 border-white' }}">
+                <div>
+                    <span class="icon-[tabler--lock] profile-second-div-wrap-two"></span>
+                </div>
+                <div>Security</div>
+            </a>
+        </div>
     </div>
 </x-trader-layout>

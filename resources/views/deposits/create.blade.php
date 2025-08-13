2449444bcecbd04b2b5acc189f578d7712a74b87
Anthony Nnanna <anthonynnanna004@gmail.com>
2025-08-13 07:09:43 +0100

    address copy

--- resources/views/deposits/create.blade.php
+++ resources/views/deposits/create.blade.php
@@ -26,7 +26,7 @@
                 <span>Copy</span>
             </div>
             <div id="address-copy-toast" class="hidden fixed bottom-5 right-5 bg-green-500 text-white px-4 py-2 rounded shadow z-50">
-                Link copied to clipboard!
+                Address copied to clipboard!
             </div>
         </div>
 

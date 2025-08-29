b3c69c1dec1538ce4f032ceeb33189f373de74f5
chiefJurist <anthonynnanna004@gmail.com>
2025-08-29 19:35:40 +0100

    seting up withdrawal migration and model

--- app/Models/Withdrawal.php
+++ app/Models/Withdrawal.php
@@ -7,6 +7,18 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Withdrawal extends Model
 {
+    protected $fillable = [
+        'user_id',
+        'amount',
+        'method',
+        'account'
+    ];
+
+    protected $casts = [
+        'amount' => 'decimal:2',
+        'date' => 'datetime',
+    ];
+    
     //
     public function user() : BelongsTo {
         return $this->belongsTo(User::class);
--- database/migrations/2025_08_29_140535_create_withdrawals_table.php
+++ database/migrations/2025_08_29_140535_create_withdrawals_table.php
@@ -14,6 +14,10 @@ return new class extends Migration
         Schema::create('withdrawals', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->decimal('amount',15,2)->nullable();
+            $table->string('method')->nullable();
+            $table->string('account')->nullable();
         });
     }
 

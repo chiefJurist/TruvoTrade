8b5bb62101403fc0b499f89470156ad12e04b23e
chiefJurist <anthonynnanna004@gmail.com>
2025-09-17 10:49:34 +0100

    CUSTOMER SUPPORT

--- resources/views/components/home-layout.blade.php
+++ resources/views/components/home-layout.blade.php
@@ -46,6 +46,19 @@
             }
         });
     </script>
+    <!--Start of Tawk.to Script-->
+    <script type="text/javascript">
+        var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+        (function(){
+        var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+        s1.async=true;
+        s1.src='https://embed.tawk.to/68ca82adb1960d1924ccd0f3/1j5bgsu0b';
+        s1.charset='UTF-8';
+        s1.setAttribute('crossorigin','*');
+        s0.parentNode.insertBefore(s1,s0);
+        })();
+    </script>
+    <!--End of Tawk.to Script-->
     <body class="bg-gray-100">
         {{-- LOADER --}}
         <div id="app-loader" class="fixed inset-0 bg-gray-300 z-50 flex items-center justify-center">

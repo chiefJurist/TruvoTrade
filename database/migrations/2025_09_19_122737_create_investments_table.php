59d8a810bd9ead363f5990eafec1e6a7aa63d08b
chiefJurist <anthonynnanna004@gmail.com>
2025-09-20 22:56:24 +0100

    create investment migration

---
+++ database/migrations/2025_09_19_122737_create_investments_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('investments', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('plan')->nullable();
+            $table->string('amount')->nullable();
+            $table->enum('status', ['pending','successful', 'failed'])->default('pending');
+            $table->string('minimum')->nullable();
+            $table->string('maximum')->nullable();
+            $table->decimal('profit', 15, 2)->default(0.00);
+            $table->timestamp('end')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('investments');
+    }
+};
